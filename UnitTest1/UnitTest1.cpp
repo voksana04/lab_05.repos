da5bd0ed9baff180f99c9fe4ddfa5c458beba000
unknown <voksana04>
2024-10-03 15:04:32 +0300

    third commit

--- UnitTest1/UnitTest1.cpp
+++ UnitTest1/UnitTest1.cpp
@@ -1,5 +1,7 @@
 #include "pch.h"
 #include "CppUnitTest.h"
+#include "../Project_5.1/lab_5.1.cpp"
+
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -13,7 +15,7 @@ namespace UnitTest1
 		{
 			double t;
 			t = k(2, 4);
-			Assert::AreEqual(k, 2.0717, 0.00001);
+			Assert::AreEqual(t, 2.0717, 0.00001);
 		}
 	};
 }
